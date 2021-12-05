93ac3ade08230186718f8937815312f8adf766ca
Muhammad Awais Shah <82607059+awaisaslam186@users.noreply.github.com>
2021-12-06 00:41:55 +0500

    session setup completed

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const express=require('express')
 const mongoose=require('mongoose')
 const cors= require('cors')
 const cookieParser=require('cookie-parser')
+const session=require('express-session')
+const flash=require('express-flash')
+const MongoStore= require ('connect-mongo')
 
 // variables
 const app=express()
 const port=process.env.PORT || 5000
 const mongo_uri=process.env.MONGO_DB_URL
+
 
 //middleware
 app.use(cors({
@@ -18,6 +22,7 @@ app.use(cors({
 }))
 app.use(cookieParser())
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 // database connection
 mongoose.connect(mongo_uri,{ useNewUrlParser: true, useUnifiedTopology: true },
   // err=>{
@@ -26,6 +31,21 @@ mongoose.connect(mongo_uri,{ useNewUrlParser: true, useUnifiedTopology: true },
 
 // }
 )
+//session config
+const db=mongoose.connection
+// let mongoStore=new MongoStore({
+//   mongooseConnection : db,
+//   collection : "sessions"
+// })
+
+app.use(session({
+  secret : process.env.SECRET_KEY,
+  resave:false,
+  store: MongoStore.create({mongoUrl: process.env.MONGO_DB_URL}),
+  saveUninitialized:false,
+  cookie: {maxAge : 1000*60*60*24}
+}))
+
 
 
 //router
@@ -35,7 +55,6 @@ app.get('/',(req,res)=>{
 app.use('/api', require("./routes/menuRouter"))
 
 // listening to port
-const db=mongoose.connection
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
   console.log("Connected successfully");
